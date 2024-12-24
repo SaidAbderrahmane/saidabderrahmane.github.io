4a88da5ad3df7d85031429697074ab337fd567df
saidabder <saidabd3@gmail.com>
2024-12-25 00:17:00 +0100

    update data

--- data/index.ts
+++ data/index.ts
@@ -1,8 +1,10 @@
+import { link } from "fs";
+
 export const navItems = [
   { name: "About", link: "#about" },
   { name: "Projects", link: "#projects" },
-  { name: "Testimonials", link: "#testimonials" },
   { name: "Contact", link: "#contact" },
+  { name: "Resume", link: "#resume" },
 ];
 
 export const gridItems = [
@@ -46,7 +48,6 @@ export const gridItems = [
     img: "/grid.svg",
     spareImg: "/b4.svg",
   },
-
   {
     id: 5,
     title: "Currently building a JS Animation library",
@@ -72,34 +73,34 @@ export const gridItems = [
 export const projects = [
   {
     id: 1,
-    title: "3D Solar System Planets to Explore",
+    title: "Ahbab El marda Website",
     des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-    img: "/p1.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    img: "/ahbab.png",
+    iconLists: ["/laravel.svg", "/tail.svg","/re.svg", "/ts.svg", "/three.svg", "/fm.svg"],
     link: "/ui.earth.com",
   },
   {
     id: 2,
-    title: "Yoom - Video Conferencing App",
-    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    title: "Delfiv Events App",
+    des: "Created a mobile app to manage events using Flutter, including features for ticketing and mailing.",
     img: "/p2.svg",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
     link: "/ui.yoom.com",
   },
   {
     id: 3,
-    title: "AI Image SaaS - Canva Application",
-    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-    img: "/p3.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    title: "OpenMarket - Multi-vendor e-commerce website",
+    des: "Developed a scalable e-commerce platform for multiple vendors using Laravel, Bootstrap CSS, and Stripe.",
+    img: "/openmarket.png",
+    iconLists: ["/laravel.svg","/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
     link: "/ui.aiimg.com",
   },
   {
     id: 4,
-    title: "Animated Apple Iphone 3D Website",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-    img: "/p4.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    title: "Logifusion",
+    des: "Data Governance app for Logicom commerce management software ",
+    img: "/logifusion.png",
+    iconLists: ["/flutter.svg", "/laravel.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
     link: "/ui.apple.com",
   },
 ];
@@ -205,13 +206,11 @@ export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
-  },
-  {
-    id: 2,
-    img: "/twit.svg",
+    link:"//github.com/saidabderrahmane"
   },
   {
     id: 3,
     img: "/link.svg",
+    link:"//linkedin.com/hadj-said-abderrahmane"
   },
 ];
